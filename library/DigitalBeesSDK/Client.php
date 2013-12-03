5906367d2bcc4f07f02701c76ec31bddc0573312
GianArb <gianarb92@gmail.com>
2013-12-03 14:15:34 +0100

    add playlist call

--- library/DigitalBeesSDK/Client.php
+++ library/DigitalBeesSDK/Client.php
@@ -25,7 +25,22 @@ class Client
         $this->apiSecret = $apiSecret;
         parent::__construct($options['host']);
     }
-    
+
+    /**
+     * I'm here, this is my configuration
+     * @return array
+     */
+    public function me()
+    {
+        $request = $this->getRequest();
+        $request->setUri("{$this->getUri()->toString()}view/user-config");
+        $request->getQuery()->apisecret = $this->apiSecret;
+        $request->getQuery()->apikey = $this->apiKey;
+        $this->setRequest($request);
+        $this->setResponse($this->send());
+        return json_decode($this->getResponse()->getBody(), true);
+    }
+
     /**
      * Return all videos
      * @return array
@@ -145,6 +160,39 @@ class Client
         $request->setUri("{$this->getUri()->toString()}get/author/{$id}");
         $request->getQuery()->apisecret = $this->apiSecret;
         $request->getQuery()->apikey = $this->apiKey;
+        $this->setRequest($request);
+        $this->setResponse($this->send());
+        return json_decode($this->getResponse()->getBody(), true);
+    }
+
+    /**
+     * list of all playlist
+     * @return array
+     */
+    public function getPlaylists()
+    {
+        $request = $this->getRequest();
+        $request->setUri("{$this->getUri()->toString()}view/playlist");
+        $request->getQuery()->apisecret = $this->apiSecret;
+        $request->getQuery()->apikey = $this->apiKey;
+
+        $this->setRequest($request);
+        $this->setResponse($this->send());
+        return json_decode($this->getResponse()->getBody(), true);
+    }
+
+    /**
+     * Video into single playlist
+     * @param int|string $id single playlist
+     * @return array
+     */
+    public function getPlaylist($id)
+    {
+        $request = $this->getRequest();
+        $request->setUri("{$this->getUri()->toString()}view/video-by-playlist");
+        $request->getQuery()->apisecret = $this->apiSecret;
+        $request->getQuery()->apikey = $this->apiKey;
+        $request->getQuery()->id = $id;
         $this->setRequest($request);
         $this->setResponse($this->send());
         return json_decode($this->getResponse()->getBody(), true);
